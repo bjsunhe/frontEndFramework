a149eb32eab99ac00b56aaad9ccd8beed2d923ad
bjsunhe <bjsunhe@sina.com>
2016-09-11 20:57:21 +0800

    books

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ Async
 RxJS  RxJS, The Reactive Extensions for JavaScript
 Cycle.js
 meng
+
+
+前端架构
+前端实战与产品设计，（用户体验，产品设计，前端技术）
+
+（前端工程师进阶）
+React，React Native与Redux架构项目实战(Redux架构详解，Webpack打包，Javascript最新规范ES6，Flex布局)
---
+++ todos.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
+const todos=(state=[],action)=>{
+  switch(action.type){
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id:action.id,
+          text:action.text,
+          completed:false
+        }
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(todo=>{
+        if(todo.id!==action.id){
+          return todo;
+        }
+        return{
+          ...todo,
+          completed:!todo.completed
+        }
+      });
+    default:
+      return state;
+  }
+}
